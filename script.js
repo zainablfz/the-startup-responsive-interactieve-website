f4344b857cc425fc5a3be194008281d5bbf6cbc9
zainablfz <144009548+zainablfz@users.noreply.github.com>
2024-01-18 17:59:12 +0100

    added content blog cards

--- script.js
+++ script.js
@@ -1,3 +1,77 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const modal = document.querySelector(".modal");
+  const overlay = document.querySelector(".overlay");
+  const closeModalBtn = document.querySelector(".btn-close");
+
+  const openModal = function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  };
+
+  const closeModal = function () {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+  };
+
+  closeModalBtn.addEventListener("click", closeModal);
+
+   overlay.addEventListener("click", closeModal);
+
+  const openModalBtns = document.querySelectorAll(".btn-open");
+  openModalBtns.forEach(function (btn) {
+    btn.addEventListener("click", openModal);
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   const modal = document.querySelector(".modal");
+//   const overlay = document.querySelector(".overlay");
+//   const openModalBtn = document.querySelector(".btn-open");
+//   const closeModalBtn = document.querySelector(".btn-close");
+
+//   const openModal = function () {
+//     modal.classList.remove("hidden");
+//     overlay.classList.remove("hidden");
+//   };
+
+//   openModalBtn.addEventListener("click", openModal);
+
+//   const closeModal = function () {
+//     modal.classList.add("hidden");
+//     overlay.classList.add("hidden");
+//   };
+
+//   closeModalBtn.addEventListener("click", closeModal);
+// });
+
+
+
+
+// document.addEventListener('DOMContentLoaded', function () {
 // const modal = document.querySelector(".modal");
 // const overlay = document.querySelector(".overlay");
 // const openModalBtn = document.querySelector(".btn-open");
@@ -16,27 +90,26 @@
 //   modal.classList.add("hidden");
 //   overlay.classList.add("hidden");
 // };
-
-document.addEventListener('DOMContentLoaded', function () {
-  // Je JavaScript-code hier
-  const modal = document.querySelector(".modal");
-  const overlay = document.querySelector(".overlay");
-  const openModalBtn = document.querySelector(".btn-open");
-  const closeModalBtn = document.querySelector(".btn-close");
-
-  const openModal = function () {
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
-  };
-
-  openModalBtn.addEventListener("click", openModal);
-
-  const closeModal = function () {
-    modal.classList.add("hidden");
-    overlay.classList.add("hidden");
-  };
-
-  closeModalBtn.addEventListener("click", closeModal);
-});
+// });
 
 
+
+
+
+// const modal = document.querySelector(".modal");
+// const overlay = document.querySelector(".overlay");
+// const openModalBtn = document.querySelector(".btn-open");
+// const closeModalBtn = document.querySelector(".btn-close");
+
+
+// openModalBtn.addEventListener("click", openModal);
+// const openModal = function () {
+//   modal.classList.remove("hidden");
+//   overlay.classList.remove("hidden");
+// };
+
+// closeModalBtn.addEventListener("click", closeModal);
+// const closeModal = function () {
+//   modal.classList.add("hidden");
+//   overlay.classList.add("hidden");
+// };
