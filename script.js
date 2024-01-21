c413be43e3b08b127e00177e6afbed41ba15bb0e
zainablfz <144009548+zainablfz@users.noreply.github.com>
2024-01-21 21:00:51 +0100

    added modal

--- script.js
+++ script.js
@@ -1,27 +1,99 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const modal = document.querySelector(".modal");
-  const overlay = document.querySelector(".overlay");
-  const closeModalBtn = document.querySelector(".btn-close");
+    const modal = document.querySelector(".modal");
+    const confirmed = document.querySelector(".confirmed");
+    const overlay = document.querySelector(".overlay");
+    const closeModalBtns = document.querySelectorAll(".btn-close");
 
-  const openModal = function () {
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
-  };
+    const openModal = function () {
+      modal.classList.remove("hidden");
+      overlay.classList.remove("hidden");
+    };
 
-  const closeModal = function () {
-    modal.classList.add("hidden");
-    overlay.classList.add("hidden");
-  };
+    const openConfirmedModal = function () {
+      modal.classList.add("hidden");
+      confirmed.classList.remove("hidden");
+    };
 
-  closeModalBtn.addEventListener("click", closeModal);
+    const closeModal = function () {
+      modal.classList.add("hidden");
+      confirmed.classList.add("hidden");
+      overlay.classList.add("hidden");
+    };
 
-   overlay.addEventListener("click", closeModal);
+    closeModalBtns.forEach(function (btn) {
+      btn.addEventListener("click", closeModal);
+    });
 
-  const openModalBtns = document.querySelectorAll(".btn-open");
-  openModalBtns.forEach(function (btn) {
-    btn.addEventListener("click", openModal);
+    overlay.addEventListener("click", closeModal);
+
+    const openModalBtns = document.querySelectorAll(".btn-open");
+    openModalBtns.forEach(function (btn) {
+      btn.addEventListener("click", openModal);
+    });
+
+    const submitBtn = document.querySelector(".submit");
+    submitBtn.addEventListener("click", openConfirmedModal);
   });
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   const modal = document.querySelector(".modal");
+//   const overlay = document.querySelector(".overlay");
+//   const submit = document.querySelector(".submit");
+//   const confirmed = document.querySelector(".confirmed");
+//   const closeModalBtn = document.querySelector(".btn-close");
+
+//   const openModal = function () {
+//     modal.classList.remove("hidden");
+//     overlay.classList.remove("hidden");
+
+//   };
+
+//   const closeModal = function () {
+//     modal.classList.add("hidden");
+//     overlay.classList.add("hidden");
+
+//   };
+
+//   closeModalBtn.addEventListener("click", closeModal);
+
+//    overlay.addEventListener("click", closeModal);
+
+//   const openModalBtns = document.querySelectorAll(".btn-open");
+//   openModalBtns.forEach(function (btn) {
+//     btn.addEventListener("click", openModal);
+//   });
+
+
+// });
 
 
 
